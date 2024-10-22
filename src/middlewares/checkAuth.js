6483a22358f9483ac8d58ee2524b69ac6d6287af
Francisco Zuidwijk <electrocisko@gmail.com>
2024-10-22 10:54:51 -0300

    punto de partida para empezar a trabajar con react en el front

--- src/middlewares/checkAuth.js
+++ src/middlewares/checkAuth.js
@@ -4,7 +4,11 @@ const checkAuth = (req, res, next) => {
     if (req.isAuthenticated()) {
         next();
     } else {
-        res.redirect("/");
+        res.status(401).json({
+            statusOk: false,
+            message: "No credentials"
+        });
+
     }
 }
 
