5b46d77ba1fb30f96b5d6e474789d97b75604c91
Francisco Zuidwijk <electrocisko@gmail.com>
2025-05-03 18:23:19 -0300

    se elimino uploadimage y se reemplazo por uploadcloudenary

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -17,13 +17,11 @@ import {
   addNewColorToProduct,
   modifiedStockById,
   getFilteredProducts,
-  queryParamsProducts
-
+  queryParamsProducts,
 } from "../controllers/productsController.js";
-import upLoader from "../middlewares/uploadImage.js";
 import checkAuth from "../middlewares/checkAuth.js";
 
-import {upload, uploadToCloudinary} from "../middlewares/upLoadCloudinary.js"
+import { upload, uploadToCloudinary } from "../middlewares/upLoadCloudinary.js";
 
 const router = express.Router();
 
@@ -32,20 +30,44 @@ router.get("/colors", getAllColors);
 router.get("/sizes", getAllSizes);
 router.get("/products/new", getNewProducts);
 router.get("/products/top", getTopProducts);
-router.get("/products/onsale",getOnsaleProducts);
-router.get("/products/women",getWomenProducts);
-router.get("/products/men",getMenProducts);
-router.get("/products/filter",getFilteredProducts);
-router.get("/products/uni",getUniProducts);
+router.get("/products/onsale", getOnsaleProducts);
+router.get("/products/women", getWomenProducts);
+router.get("/products/men", getMenProducts);
+router.get("/products/filter", getFilteredProducts);
+router.get("/products/uni", getUniProducts);
 router.get("/product/:id", getProductById);
-router.get("/products/querys/",queryParamsProducts)
+router.get("/products/querys/", queryParamsProducts);
 
 // private routes
-router.post("/product",checkAuth, upload.single("imageurl"), uploadToCloudinary, createNewproduct);
-router.post("/fullproduct", checkAuth,  upload.single("imageurl"), uploadToCloudinary, addNewFullProduct);
-router.post("/stock", checkAuth,  upload.single("imageurl"), uploadToCloudinary, addStock);
-router.post("/addcolor",checkAuth,   upload.single("imageurl"), uploadToCloudinary, addNewColorToProduct);
-router.put("/products/updatestock",checkAuth, modifiedStockById);
-router.delete("/product/:id",checkAuth, deleteProductById);
+router.post(
+  "/product",
+  checkAuth,
+  upload.single("imageurl"),
+  uploadToCloudinary,
+  createNewproduct
+);
+router.post(
+  "/fullproduct",
+  checkAuth,
+  upload.single("imageurl"),
+  uploadToCloudinary,
+  addNewFullProduct
+);
+router.post(
+  "/stock",
+  checkAuth,
+  upload.single("imageurl"),
+  uploadToCloudinary,
+  addStock
+);
+router.post(
+  "/addcolor",
+  checkAuth,
+  upload.single("imageurl"),
+  uploadToCloudinary,
+  addNewColorToProduct
+);
+router.put("/products/updatestock", checkAuth, modifiedStockById);
+router.delete("/product/:id", checkAuth, deleteProductById);
 
 export default router;
